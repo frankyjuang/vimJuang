4c99aa325e7656b84e7eeee4daddbfedc0421953
Yi-Lin Juang <frankyjuang@gmail.com>
2015-12-28 19:08:49 +0800

    [Fix installation]

--- install.sh
+++ install.sh
@@ -1,21 +1,10 @@
-###Install Configurations###
-
-echo "Installing vimJuang Configurations..."
-
 echo 'set runtimepath+=~/.vimJuang
 
 source ~/.vimJuang/vimrcs/basic.vim
 source ~/.vimJuang/vimrcs/filetypes.vim
 source ~/.vimJuang/vimrcs/plugins_config.vim' > ~/.vimrc
 
-echo "Successfully Install vimJuang Configurations!!!"
-
-###Install Plugins###
-
-echo "Installing Plugins..."
+mkdir -p ~/.vimJuang/plugins
+git clone https://github.com/VundleVim/Vundle.vim ~/.vimJuang/plugins/Vundle.vim
 
 vim +PluginInstall +qall
-
-echo "Successfully Install Plugins!!!"
-
-echo "Enjoy~~~!!!"
