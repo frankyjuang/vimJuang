7e175a6a0950eae18673425d6965ed1ac01f720f
Yi-Lin Juang <frankyjuang@gmail.com>
2016-01-02 20:37:20 +0800

    [Fix install.sh]
    Check if Vundle exists before git clone it.

--- install.sh
+++ install.sh
@@ -4,9 +4,16 @@ source ~/.vimJuang/vimrcs/basic.vim
 source ~/.vimJuang/vimrcs/filetypes.vim
 source ~/.vimJuang/vimrcs/plugins_config.vim' > ~/.vimrc
 
-mkdir -p ~/.vimJuang/plugins
-git clone https://github.com/VundleVim/Vundle.vim ~/.vimJuang/plugins/Vundle.vim
+if [ -f plugins/Vundle.vim/README.md ]; then
+    echo "Vundle alreadly exists!"
+else
+    echo "Install Vundle\n"
+    mkdir -p ~/.vimJuang/plugins
+    git clone https://github.com/VundleVim/Vundle.vim ~/.vimJuang/plugins/Vundle.vim
+fi
 
+echo "Install Plugins\n"
 vim +PluginInstall +qall
 
-echo 'vimJuang install completed'
+echo "VimJuang Installed Successfully! Enjoy~"
+echo "Any problems please contact Yi-Lin Juang (frankyjuang@gmail.com)"
