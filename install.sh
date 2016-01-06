3e2c34dce2f9c99cdd2318c5db35cb47c47fa4ff
Yi-Lin Juang <frankyjuang@gmail.com>
2016-01-06 12:44:56 +0800

    Fix install.sh

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ source ~/.vimJuang/vimrcs/filetypes.vim
 source ~/.vimJuang/vimrcs/plugins_config.vim' > ~/.vimrc
 
 if [ -f plugins/Vundle.vim/README.md ]; then
-    echo "Vundle alreadly exists!"
+    printf "Vundle alreadly exists!\n"
 else
-    echo "Install Vundle\n"
+    printf "Install Vundle\n"
     mkdir -p ~/.vimJuang/plugins
     git clone https://github.com/VundleVim/Vundle.vim ~/.vimJuang/plugins/Vundle.vim
 fi
 
-echo "Install Plugins\n"
+printf "Install Plugins\n"
 vim +PluginInstall +qall
 
-echo "VimJuang Installed Successfully! Enjoy~"
-echo "Any problems please contact Yi-Lin Juang (frankyjuang@gmail.com)"
+printf "VimJuang Installed Successfully! Enjoy~\n"
+printf "Any problems please contact Yi-Lin Juang (frankyjuang@gmail.com)\n"
